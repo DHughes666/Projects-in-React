4eeed02e29971faf613345304e9bba6ad864268e
DHughes666 <graziemolto6@gmail.com>
2023-10-07 19:36:28 +0100

    Ternary operator

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,12 +1,12 @@
 import OrderMatters from "./tutorial/03-conditional-rendering/02-order-matters-component"
 import ShortCircuitOverview from "./tutorial/03-conditional-rendering/03-short-circuit-component"
-
+import ShortCircuitExamples from "./tutorial/03-conditional-rendering/04-short-circuit-examples-component"
 
 function App() {
   return (
     <div className='container'>
-      <OrderMatters />
-      <ShortCircuitOverview />
+      {/* <OrderMatters /> */}
+      <ShortCircuitExamples />
     </div>
     )
 }
