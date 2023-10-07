c0d962e52059425e871abcc91b2afb7d9611d8ac
DHughes666 <graziemolto6@gmail.com>
2023-10-07 19:09:52 +0100

    Truth & False conditions - vanilla js

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,8 +1,12 @@
 import OrderMatters from "./tutorial/03-conditional-rendering/02-order-matters-component"
+import ShortCircuitOverview from "./tutorial/03-conditional-rendering/03-short-circuit-component"
+
+
 function App() {
   return (
     <div className='container'>
       <OrderMatters />
+      <ShortCircuitOverview />
     </div>
     )
 }
