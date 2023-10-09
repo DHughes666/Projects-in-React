5490db9d7b3bd4188c92b08d10a6a53ce145ead0
DHughes666 <graziemolto6@gmail.com>
2023-10-10 00:30:10 +0100

    Uncontrolled inputs -- FormData -- objects

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import ControlledInputs from "./tutorial/05-controlled-inputs/01-controlled-inpu
 import ControlledInputChallenge from "./tutorial/05-controlled-inputs/02-controlled-inputs-challenge-component"
 import MultipleInputs from "./tutorial/05-controlled-inputs/03-multiple-inputs-component"
 import OtherInputs from "./tutorial/05-controlled-inputs/04-otherInputs-component"
+import UncontrolledInputs from "./tutorial/05-controlled-inputs/05-formData-component"
 
 function App() {
   return (
     <div className='container'>
+      <UncontrolledInputs />
       <OtherInputs />
       <MultipleInputs />
       <ControlledInputs />
---
+++ vite-react-app/src/tutorial/05-controlled-inputs/05-formData-component.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const UncontrolledInputs = () => {
+    const [value, setValue] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        console.log(formData);
+        const email = formData.get('email');
+        console.log(email);
+    };
+
+    return (
+        <div>
+            <form className="form" onSubmit={handleSubmit}>
+                <h4>Form Data API</h4>
+                <div className="form-row">
+                    <label htmlFor="name" className="form-label">
+                        name
+                    </label>
+                    <input 
+                    type="text" 
+                    name="name"
+                    className="form-input" />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="email" className="form-label">
+                        Email
+                    </label>
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email"
+                    className="form-input" /> 
+                </div>
+                <div className="form-row">
+                    <label htmlFor="password" className="form-label">
+                        Password
+                    </label>
+                    <input 
+                    type="password"
+                    name="password"
+                    className="form-input" />
+                </div>
+                <button type="submit" className="btn btn-block">
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default UncontrolledInputs;
