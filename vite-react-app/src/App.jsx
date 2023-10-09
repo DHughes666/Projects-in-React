df16c8c72f538a5ff1e844dd721bd2adad0429bb
DHughes666 <graziemolto6@gmail.com>
2023-10-10 00:58:50 +0100

    UseRef -basics

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,17 +1,14 @@
-import ControlledInputs from "./tutorial/05-controlled-inputs/01-controlled-inputs-component"
-import ControlledInputChallenge from "./tutorial/05-controlled-inputs/02-controlled-inputs-challenge-component"
-import MultipleInputs from "./tutorial/05-controlled-inputs/03-multiple-inputs-component"
 import OtherInputs from "./tutorial/05-controlled-inputs/04-otherInputs-component"
 import UncontrolledInputs from "./tutorial/05-controlled-inputs/05-formData-component"
+import UseRefBasics from "./tutorial/06-useRefHook/01-useRef-Basics-component"
 
 function App() {
   return (
     <div className='container'>
+      <UseRefBasics />
       <UncontrolledInputs />
       <OtherInputs />
-      <MultipleInputs />
-      <ControlledInputs />
-      <ControlledInputChallenge />
+      
     </div>
     )
 }
