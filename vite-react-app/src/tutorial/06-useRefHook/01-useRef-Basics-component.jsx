9c2be286bcaa2623f10ac30cde6744741b838f97
DHughes666 <graziemolto6@gmail.com>
2023-10-10 01:09:12 +0100

    UseRef -basics ...

--- vite-react-app/src/tutorial/06-useRefHook/01-useRef-Basics-component.jsx
+++ vite-react-app/src/tutorial/06-useRefHook/01-useRef-Basics-component.jsx
@@ -3,11 +3,20 @@ import { useEffect, useRef, useState } from 'react';
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
-  console.log(refContainer);
+  const isMounted = useRef(false);
+//   console.log(refContainer);
 
-//   useEffect(() => {
-//     console.log(refContainer);
-//   })
+  useEffect(() => {
+    refContainer.current.focus();
+  })
+
+  useEffect(() => {
+    if (!isMounted.current) {
+        isMounted.current = true;
+        return;
+    }
+    console.log('re-render');
+  }, [value]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
