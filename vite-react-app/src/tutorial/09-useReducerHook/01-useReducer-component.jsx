82bf36f196df9d31398426fcf6a5dd693418ee9e
DHughes666 <graziemolto6@gmail.com>
2023-10-13 05:52:12 +0100

    useReducer --- on Point git add .!  - cutely refactored

--- vite-react-app/src/tutorial/09-useReducerHook/01-useReducer-component.jsx
+++ vite-react-app/src/tutorial/09-useReducerHook/01-useReducer-component.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import { useReducer } from 'react';
 import { data } from '../../data';
+import {CLEAR_LIST, RESET_LIST, REMOVE_ITEM} from './actions';
+import reducer from './reducer';
+
+const defaultState = {
+    people: data,
+    isLoading: false,
+}
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+
+    const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({type: REMOVE_ITEM, payload: {id}});
   };
 
   const clearList = () => {
-    setPeople([]);
+    dispatch({type: CLEAR_LIST});
   }
 
   const resetList = () => {
-    setPeople(data);
+    dispatch({type: RESET_LIST})
   }
 
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -28,7 +36,7 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length < 1 ? (
+      {state.people.length < 1 ? (
         <button
         className='btn'
         style={{ marginTop: '2rem' }}
