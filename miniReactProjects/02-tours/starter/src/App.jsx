7fc956f9a6bd061c13cdc1cfcd0e7a64cb97a8c1
DHughes666 <graziemolto6@gmail.com>
2023-10-14 22:17:30 +0100

    Tours project completed

--- miniReactProjects/02-tours/starter/src/App.jsx
+++ miniReactProjects/02-tours/starter/src/App.jsx
@@ -1,6 +1,60 @@
+import { useState, useEffect } from "react";
+import Loading from "./loading";
+import Tours from "./tours";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+    
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if(tours.length < 1) {
+    return <main>
+      <div className="title">
+        <h2>No tours left</h2>
+        <button onClick={() => fetchTours()} className="btn btn-block">
+         Refresh
+        </button>
+      </div>
+    </main>
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  )
 };
 export default App;
---
+++ miniReactProjects/02-tours/starter/src/tours.jsx
@@ -0,0 +1,21 @@
+import Tour from "./tour";
+
+const Tours = ({tours, removeTour}) => {
+    return (
+        <section>
+            <div className="title">
+                <h2>Our Tours</h2>
+                <div className="title-underline" />
+            </div>
+            <div>
+                {tours.map((tour) => {
+                    return <Tour key={tour.id} {...tour}
+                        removeTour={removeTour}
+                    />
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Tours;
