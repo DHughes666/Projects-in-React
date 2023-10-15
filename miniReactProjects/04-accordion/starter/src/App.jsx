306442d6e4c7c5b56bebb983cdf289c892341b1c
DHughes666 <graziemolto6@gmail.com>
2023-10-15 09:59:12 +0100

    Accordion project ...underway

--- miniReactProjects/04-accordion/starter/src/App.jsx
+++ miniReactProjects/04-accordion/starter/src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+import data from './data';
+import Question from './singleQuestion';
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions and answers about login</h3>
+        <br />
+        <section className='info'>
+          {
+            questions.map((question) => {
+              return <Question key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
+    </main>
+  )
 };
 export default App;
---
+++ miniReactProjects/04-accordion/starter/src/singleQuestion.jsx
@@ -0,0 +1,14 @@
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai';
+
+const Question = ({title, info}) => {
+    return (
+        <article className='question'>
+            <header>
+                <h4>{title}</h4>
+                <button className='btn'>btn</button>
+            </header>
+        </article>
+    )
+}
+
+export default Question;
