aaaf28522238062424a1861b152136be5bb14c6b
DHughes666 <graziemolto6@gmail.com>
2023-10-17 20:55:14 +0100

    Fixed rendering 'Used <div> instead of <article>

--- miniReactProjects/07-slider/starter/src/App.jsx
+++ miniReactProjects/07-slider/starter/src/App.jsx
@@ -1,13 +1,46 @@
 import { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import data from './data'
 
-const App = () => {
+function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+  
+  return <section className='section'>
+    <div className='title'>
+      <h2>
+        <span>/</span>reviews
+      </h2>
+    </div>
+    <div className='section-center'>
+      {people.map((person, personIndex) => {
+        const {id, image, name, title, quote} = person
+        // Do something 
 
-  return (<h2>Slider setup</h2>)
+        return (
+          <div key={id}>
+            <img src={image} alt={name} className='person-img'/>
+            <h4>{name}</h4>
+            <p className='title'>{title}</p>
+            <p className='text'>{quote}</p>
+            <button className='prev'>
+              <FiChevronLeft />
+            </button>
+            <button className='next'>
+              <FiChevronRight />
+            </button>
+            <button>
+              <FaQuoteRight />
+            </button>
+
+          </div>
+        )
+        
+      })}
+    </div>
+  </section>
 }
 
 export default App;
+  
