43c810eb28055b2ff2a908d3386897b7c1b85ef2
DHughes666 <graziemolto6@gmail.com>
2023-10-20 04:23:42 +0100

    Color gen ...In progregress

--- miniReactProjects/09-color-generator/starter/src/App.jsx
+++ miniReactProjects/09-color-generator/starter/src/App.jsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from 'values.js';
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
-};
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (err) {
+      setError(true)
+    }
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} 
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />
+        })}
+      </section>
+    </>
+  )
+}
+
 export default App;
