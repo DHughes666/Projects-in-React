dcdac8da603936ea46a18bd1e0197d7c096f9715
DHughes666 <graziemolto6@gmail.com>
2023-10-20 21:55:14 +0100

    Grocery bud be budding goodiesgit add .!

--- miniReactProjects/10-grocery-bud/starter/src/App.jsx
+++ miniReactProjects/10-grocery-bud/starter/src/App.jsx
@@ -2,9 +2,19 @@ import { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  } else {
+    return [];
+  }
+}
+
 const App = () => {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -18,21 +28,55 @@ const App = () => {
       showAlert(true, 'danger', 'Please enter value')
     } else if (name && isEditing) {
       // deal with edit
+      setList(list.map((item) => {
+        if (item.id === editID) {
+          return {...item, title: name}
+        }
+        return item
+      }))
+      setName('')
+      setEditID(null);
+      setIsEditing(false)
+      showAlert(true, 'info', 'Item successfully edited')
     } else {
       // show alert
+      showAlert(true, 'success', 'item added to the list')
       const newItem = {id: new Date().getTime().toString(), title:name};
       setList([...list, newItem]);
       setName('');
     }
   }
 
+  const removeItem = (itemID) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter(item => item.id !== itemID));
+  }
+
+  const editItem = (itemID) => {
+    const editedItem = list.find(item => item.id === itemID);
+    setIsEditing(true);
+    setEditID(itemID);
+    setName(editedItem.title);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list])
+
   const showAlert = (show=false, type='', msg='') => {
     setAlert({show, type, msg});
   }
 
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list')
+    setList([])
+    setIsEditing(false);
+  }
+
   return <section className="section-center">
       <form action="" className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        {alert.show && <Alert {...alert} 
+          removeAlert={showAlert} list={list}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className="grocery"
@@ -46,8 +90,8 @@ const App = () => {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list}/>
-        <button className="clear-btn">clear items</button>
+          <List items={list} removeItem={removeItem} edit={editItem}/>
+        <button className="clear-btn" onClick={clearList}>clear items</button>
     </div>
       )}
   </section>
--- miniReactProjects/10-grocery-bud/starter/src/List.jsx
+++ miniReactProjects/10-grocery-bud/starter/src/List.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
+
+
+const List = ({items, removeItem, edit}) => {
   return <div className='grocery-list'>
     {items.map((item) => {
       const {id, title} = item;
       return <article key={id} className='grocery-item'>
         <p className='title'>{title}</p>
         <div className='btn-container'>
-          <button type='button' className='edit-btn'>
+          <button type='button' className='edit-btn'
+          onClick={() => edit(id)}>
             <FaEdit />
           </button>
-          <button type='button' className='delete-btn'>
+          <button type='button' className='delete-btn' 
+          onClick={() => removeItem(id)}>
             <FaTrash />
           </button>
         </div>
