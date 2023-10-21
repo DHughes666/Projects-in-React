387ab6de366d79e098cf76d6ae8638b243231ccd
DHughes666 <graziemolto6@gmail.com>
2023-10-21 01:17:55 +0100

    Sidebar project ...Initiated

--- miniReactProjects/12-sidebar/starter/src/App.jsx
+++ miniReactProjects/12-sidebar/starter/src/App.jsx
@@ -1,4 +1,15 @@
-const App = () => {
-  return <h2>Sidebar Starter</h2>;
-};
-export default App;
+import React from 'react'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
---
+++ miniReactProjects/12-sidebar/starter/src/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+const Modal = () => {
+  return <div className={`modal-overlay show-modal`}>
+    <div className='modal-container'>
+      <h3>modal content</h3>
+    </div>
+  </div>
+}
+
+export default Modal
