5288f1f6882cb746b9c4176102422038e8b80daa
DHughes666 <graziemolto6@gmail.com>
2023-10-21 10:24:55 +0100

    Stripe project ...A lot to take in... but very interesting

--- miniReactProjects/13-stripe/src/Navbar.jsx
+++ miniReactProjects/13-stripe/src/Navbar.jsx
@@ -5,6 +5,16 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3
+    
+    openSubmenu(page, {center, bottom});
+  }
+
   return <nav className='nav'>
     <div className='nav-center'>
       <div className='nav-header'>
@@ -15,17 +25,17 @@ const Navbar = () => {
       </div>
       <ul className='nav-links'>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             products
           </button>
         </li>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             developers
           </button>
         </li>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             company
           </button>
         </li>
