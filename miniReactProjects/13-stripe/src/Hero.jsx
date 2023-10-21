abb7b9b874b5381e93a647b64f0f8d1cc01460fc
DHughes666 <graziemolto6@gmail.com>
2023-10-21 10:35:14 +0100

    Stripe project ...Completed

--- miniReactProjects/13-stripe/src/Hero.jsx
+++ miniReactProjects/13-stripe/src/Hero.jsx
@@ -3,9 +3,9 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  const {closeSubmenu} = useGlobalContext();
+  const { closeSubmenu } = useGlobalContext();
   
-  return <section className='hero'>
+  return <section className='hero' onMouseOver={closeSubmenu}>
     <div className='hero-center'>
       <article className='hero-info'>
         <h1>Payments infrastructure for the internet</h1>
--- miniReactProjects/13-stripe/src/Navbar.jsx
+++ miniReactProjects/13-stripe/src/Navbar.jsx
@@ -15,7 +15,13 @@ const Navbar = () => {
     openSubmenu(page, {center, bottom});
   }
 
-  return <nav className='nav'>
+  const handleSubmenu = (e) => {
+    if(!e.target.classList.contains('link-btn')) {
+      closeSubmenu();
+    }
+  }
+
+  return <nav className='nav' onMouseOver={handleSubmenu}>
     <div className='nav-center'>
       <div className='nav-header'>
         <img src={logo} alt='stripee' className='nav-logo'/>
