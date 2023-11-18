d3ced7c6c6f5787df46135fffa1dc76edc722699
DHughes666 <graziemolto6@gmail.com>
2023-11-18 05:22:02 +0100

    Axios-project ...data fetching (axios.get)...successful

--- axios_project/src/App.js
+++ axios_project/src/App.js
@@ -1,8 +1,11 @@
+import Setup from "./examples/1-first-request";
+
 import Title from './components/Title';
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
