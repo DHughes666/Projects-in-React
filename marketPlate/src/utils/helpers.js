1d9e8cda661aa04151d29bcb0f908e151a4e4b03
DHughes666 <graziemolto6@gmail.com>
2023-11-24 20:32:06 +0100

    format price...Naira ...really cool

--- marketPlate/src/utils/helpers.js
+++ marketPlate/src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    const newNum = Intl.NumberFormat('en-NG', {
+        style: 'currency',
+        currency: 'NGN',
+    }).format(number / 100)
+    return newNum
+}
 
 export const getUniqueValues = () => {}
