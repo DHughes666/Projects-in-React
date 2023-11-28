cc59bd2e25a7e78496c054a5e240c66772b49bb8
DHughes666 <graziemolto6@gmail.com>
2023-11-28 02:33:04 +0100

    Filtering ---Text ...Implemented. So coolgit add .!

--- marketPlate/src/reducers/filter_reducer.js
+++ marketPlate/src/reducers/filter_reducer.js
@@ -58,7 +58,19 @@ const filter_reducer = (state, action) => {
       return {...state, filters: {...state.filters, [name]: value}}
     }
     case FILTER_PRODUCTS: {
-      return {...state};
+      const {all_products} = state
+      const {text, category, company, color, price, shipping} = state.filters
+
+      let tempProducts = [...all_products]
+
+      // filtering begins
+      if(text) {
+        tempProducts = tempProducts.filter((product)=> {
+          return product.name.toLowerCase().startsWith(text)
+        })
+      }
+
+      return {...state, filtered_products: tempProducts};
     }
     case CLEAR_FILTERS: {
       return {...state, 
