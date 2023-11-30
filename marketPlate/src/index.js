f16b5731bcdb0fd9c94886e1f48b3d9c89503409
DHughes666 <graziemolto6@gmail.com>
2023-11-30 09:31:25 +0100

    Authentication ...Initiated

--- marketPlate/src/index.js
+++ marketPlate/src/index.js
@@ -9,14 +9,24 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev-xaq733ej1dy1ka5e.us.auth0.com
+// Cn3b6PMWepzKv2GdlFTCUWJNAKfiQL3r
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ProductsProvider>
-        <FilterProvider>
-            <CartProvider>
-                <App />
-            </CartProvider>
-        </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+        domain='dev-xaq733ej1dy1ka5e.us.auth0.com'
+        clientId='Cn3b6PMWepzKv2GdlFTCUWJNAKfiQL3r'
+        redirectUri={window.location.origin}
+        cacheLocation='localstorage'
+    >
+        <ProductsProvider>
+            <FilterProvider>
+                <CartProvider>
+                    <App />
+                </CartProvider>
+            </FilterProvider>
+        </ProductsProvider>
+    </Auth0Provider>
 ); 
