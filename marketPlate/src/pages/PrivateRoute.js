600eab33c9305b45e57b999eb58658407871e0e6
DHughes666 <graziemolto6@gmail.com>
2023-12-01 19:04:15 +0100

    Fixed warnings

--- marketPlate/src/pages/PrivateRoute.js
+++ marketPlate/src/pages/PrivateRoute.js
@@ -1,15 +1,10 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
-import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({children}) => {
   const navigate = useNavigate()
   const {user} = useAuth0()
-  if(!user) {
-    return navigate('/');
-  }
-  return children
+  return user ? children : navigate('/')
 };
 export default PrivateRoute;
