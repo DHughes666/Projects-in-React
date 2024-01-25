38204dd08194ffc27eaf5e6926eabf4ecfede5ab
DHughes666 <graziemolto6@gmail.com>
2024-01-25 19:14:00 +0100

    Sidebar ...In progress

--- jobss/src/components/smallSideBar.jsx
+++ jobss/src/components/smallSideBar.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { FaTimes } from 'react-icons/fa'
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import Logo from "./logo";
+import { toggleSidebar } from "../features/user/userSlice";
 
 const SmallSideBar = () => {
+    const { isSidebarOpen } = useSelector((store) => store.user);
+    const dispatch = useDispatch();
+
+    const toggle = () => {
+        dispatch(toggleSidebar());
+    }
+
     return (
         <Wrapper>
-            <h2>Small sidebar</h2>
+            <div 
+                className={
+                    isSidebarOpen ? 
+                        'sidebar-container show-sidebar' : 'sidebar-container'
+                }>
+                <div className="content">
+                    <button
+                        className="close-btn"
+                        onClick={toggle}
+                    >
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        nav links
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     )
 }
