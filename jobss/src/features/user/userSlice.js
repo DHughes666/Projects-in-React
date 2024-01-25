977bff3e509881620b6ae3131a3fd576b7e8715f
DHughes666 <graziemolto6@gmail.com>
2024-01-25 01:16:13 +0100

    Working with data on servers ...using axios

--- jobss/src/features/user/userSlice.js
+++ jobss/src/features/user/userSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
 
+import customFetch from '../../utils/axios';
+
 
 const initialState = {
     isLoading: false,
@@ -10,7 +12,13 @@ const initialState = {
 export const registerUser = createAsyncThunk(
     'user/registerUser',
     async (user, thunkAPI) => {
-        console.log(`Register User: ${JSON.stringify(user)}`);
+        
+        try {
+            const resp = await customFetch.post('/auth/testingRegister', user);
+            console.log(resp);
+        } catch (err) {
+            console.log(err.message);
+        }
     }
 );
 
