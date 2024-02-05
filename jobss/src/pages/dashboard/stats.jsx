83e25285e40950f08980c785270c4167937e4532
DHughes666 <graziemolto6@gmail.com>
2024-02-05 13:08:28 +0100

    Stats feature ...In progress

--- jobss/src/pages/dashboard/stats.jsx
+++ jobss/src/pages/dashboard/stats.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
 
 import Wrapper from "../../assets/wrappers/StatsContainer";
+import { showStats } from "../../features/allJobs/allJobs";
 
 
 const Stats = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(showStats());
+    },[]);
+
     return (
-        <Wrapper></Wrapper>
+        <Wrapper>Stats</Wrapper>
     )
 }
 
