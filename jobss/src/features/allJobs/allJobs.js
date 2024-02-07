05db0e2952132ba20941e1c32e49a556499f2ebe
DHughes666 <graziemolto6@gmail.com>
2024-02-07 04:41:07 +0100

    Donne

--- jobss/src/features/allJobs/allJobs.js
+++ jobss/src/features/allJobs/allJobs.js
@@ -75,7 +75,7 @@ const allJobsSlice = createSlice({
             state.isLoading = false;
         },
         handleChange: (state, {payload: {name, value}}) => {
-            // state.page = 1
+            state.page = 1
             state[name] = value;
         },
         clearFilters: (state) => {
