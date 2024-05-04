81ee67cd316a40235dae9e2db4be212f74639efc
DHughes666 <graziemolto6@gmail.com>
2024-05-04 01:30:55 +0100

    Markdown_Project ...Initiated

---
+++ practical_projects/markdown/src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import ReactMarkdown from 'react-markdown'
+
+function App() {
+  return <h2>markdown preview starter</h2>
+}
+
+export default App
