96b7f44f6234679f698ff4e28ea131148eb544ce
DHughes666 <graziemolto6@gmail.com>
2024-05-04 09:47:15 +0100

    Photos_Project ...So far so good

--- practical_projects/photos/src/App.jsx
+++ practical_projects/photos/src/App.jsx
@@ -17,7 +17,8 @@ function App() {
         try {
             const response = await fetch(url)
             const data = await response.json()
-            console.log(data);
+            setPhotos(data)
+            setLoading(false)
         } catch (e) {
             setLoading(false);
             console.log(e);
@@ -28,7 +29,39 @@ function App() {
         fetchImages();
     }, [])
 
-    return <h2>stock photos starter</h2>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+    return (
+        <main>
+            <section className='search'>
+                <form className='search-form'>
+                    <input
+                        type='text'
+                        placeholder='search'
+                        className='form-input'
+                    />
+                    <button 
+                    type='submit' 
+                    className='submit-btn'
+                    onClick={handleSubmit}>
+                        <FaSearch />
+                    </button>
+                </form>
+            </section>
+            <section className='photos'>
+                <div className='photos-center'>
+                    {photos.map((image) => {
+                        console.log(image);
+                        return <Photo key={image.id} {...image}/>;
+                    })}
+                </div>
+                {loading && <h2 className='loading'>Loading...</h2>}
+            </section>
+        </main>
+    )
 }
 
 export default App
